789f8b6001c3647b609117cc465152d65c0102d4
Zach Rider <zrider99zr@gmail.com>
2017-11-27 11:44:39 -0500

    Removed content type catch

--- api/index.php
+++ api/index.php
@@ -13,35 +13,27 @@ $db = Database::getConnection();
 
 $VALID_REQUESTS = array('login','logout', 'register');
 
-$contentType = isset($_SERVER["Content-Type"]) ? trim($_SERVER["Content-Type"]) : '';
-if(strcasecmp($contentType, 'application/json') != 0){
-    echo json_encode(array(
-        'message' => 'Content type must be: application/json'
-    ));
+//Receive the RAW post data.
+$content = trim(file_get_contents("php://input"));
+
+//Attempt to decode the incoming RAW post data from JSON.
+$decoded = json_decode($content, true);
+    
+if(!is_array($decoded)){
+  echo json_encode(array(
+    'message' => 'Received content contained invalid JSON!'
+  ));
 }
 else{
-    //Receive the RAW post data.
-    $content = trim(file_get_contents("php://input"));
-
-    //Attempt to decode the incoming RAW post data from JSON.
-    $decoded = json_decode($content, true);
-    
-    if(!is_array($decoded)){
-      echo json_encode(array(
-        'message' => 'Received content contained invalid JSON!'
-      ));
-    }
-    else{
-      $request = $decoded['request'];
-      if(isset($request)){
-        
-        require_once(__DIR__ . '/requests/' . $request . '.php');
-      }
-      else{
-        echo json_encode(array(
-          'message' => 'Request not sent'
-        ));
-      }
-    }
+  $request = $decoded['request'];
+  if(isset($request)){
+    require_once(__DIR__ . '/requests/' . $request . '.php')
+  }
+  else{
+    echo json_encode(array(
+      'message' => 'Request not sent'
+    ));
+  }
 }
+
  ?>
