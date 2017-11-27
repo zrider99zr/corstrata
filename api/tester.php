de23d3e15ebf3e868f1cd157bab2fd35b2a4cfa7
Zach Rider <zrider99zr@gmail.com>
2017-11-26 20:36:28 -0500

    Changed it up a bit

--- api/tester.php
+++ api/tester.php
@@ -25,7 +25,7 @@ else{
         $email = $decoded['email'];
         if(isset($request)){
             $array = array();
-            $message = "login " + $request;
+            $message = "login " . $request;
             $array['message'] = $message;
             echo json_encode($array);
         }
