93034ff0067e6d3b6151d3fe418fb2bc70819ca2
Zach Rider <zrider99zr@gmail.com>
2017-11-26 21:16:21 -0500

    index

--- api/index.php
+++ api/index.php
@@ -31,7 +31,13 @@ else{
       $request = $decoded['request'];
       if(isset($request)){
         $access = true;
-        require_once(__DIR__ . '/requests/login.php');
+        $array = array();
+        $email = $decoded['email'];
+        $password = $decoded['password'];
+        $message = "Request: " . $request . " Login: " . $email . " Password: " . $password;
+        $array['message'] = $message;
+        echo json_encode($array);
+        //require_once(__DIR__ . '/requests/login.php');
       }
       else{
         echo json_encode(array(
