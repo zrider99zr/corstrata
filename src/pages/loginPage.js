e819ad3597f83c5d092fa3b5adab11a53d866322
Zach Rider <zrider99zr@gmail.com>
2017-11-27 11:45:55 -0500

    Trying this

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -37,7 +37,6 @@ class loginPage extends Component {
             method: 'POST',
             headers: {
                 'Accept': 'application/.json',
-                'Content-Type': 'application/json',
             },
             body: JSON.stringify({
                 request: 'login',
