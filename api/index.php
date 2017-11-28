a0250777182d78efa430432355f10afac10b6c26
Zach Rider <zrider99zr@gmail.com>
2017-11-28 14:43:04 -0500

    a

--- api/index.php
+++ api/index.php
@@ -1,5 +1,6 @@
 <?php
 header("Content-Type: application/json");
+header('Access-Control-Allow-Origin: *');
 require_once(__DIR__ . '/config/global.php');
 function __autoload($className){
   require_once(__DIR__ . '/classes/' . $className . '.php');
