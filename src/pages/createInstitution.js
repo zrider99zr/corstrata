27cd63960313631c8b1b1e27c10bddcba25bc610
Zach Rider <zrider99zr@gmail.com>
2017-11-28 19:58:57 -0500

    Removed Content-Type header

--- src/pages/createInstitution.js
+++ src/pages/createInstitution.js
@@ -37,7 +37,6 @@ class createInstitution extends Component {
             method: 'POST',
             headers: {
                 'Accept': 'application/.json',
-                'Content-Type': 'application/json',
             },
             body: JSON.stringify({
                 request: 'createInstitution',
