792e56dc73ff0709b90d412b1fac6c1a700cea81
Michael Henning <mhenning@axio.com>
2017-11-28 20:19:30 -0500

    login function

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -50,6 +50,9 @@ class loginPage extends Component {
                 alert(error.message);
             })
             .done();
+            if('status'==1){
+                
+            }
     }
 
     render() {
