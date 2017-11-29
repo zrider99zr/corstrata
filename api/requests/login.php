ff2afc9fa8158a149b614feaae753f024e0b617a
zride <zrider99zr@gmail.com>
2017-11-29 14:39:05 -0500

    Fixed the thing i deleted

--- api/requests/login.php
+++ api/requests/login.php
@@ -35,7 +35,29 @@ function login($email, $password, $db){
     }
 }
 
-
+function getUsername($userID,$db){
+    if($qry = $db->prepare("SELECT firstName, lastName FROM account WHERE accountID = ?")){
+        $qry->bind_param("i",$userID);
+        $qry->execute();
+        $qry->bind_result($firstName,$lastName);
+        $qry->fetch();
+        if(isset($firstName, $lastName)){
+            $qry->close();
+            return $firstName . " " . $lastName;
+        }
+        else{
+            $qry->close();
+            return -1;
+        }
+    }
+    else{
+        $array = array();
+        $array['message'] = "query prepare uncsuccessful:(" . $db->errno . ") " . $db->error;
+        $array['status'] = 0;
+        echo json_encode($array); 
+        return -1;
+    }
+}
 //USAGE
 //Send a json with request field login and fields shown below filled
 
@@ -49,7 +71,8 @@ if(isset($email, $password)){
         $array['message'] = "Login was successful";
         $array['status'] = 1;
         $array['uid'] = $userID;
-        $session->handleSID($userID);
+        $array['name'] = getUsername($userID);
+        $array['sid'] = $session->handleSID($userID);
         echo json_encode($array);
     }
     else{
