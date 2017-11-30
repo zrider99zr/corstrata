0f7829309b3d6530e4bc60fa8b09477dc5a61483
MarleyKreps <33332461+MarleyKreps@users.noreply.github.com>
2017-11-29 23:34:49 -0500

    11/29/17
    added functionality to login button in the header.js file

--- src/pages/home.js
+++ src/pages/home.js
@@ -29,9 +29,9 @@ class Home extends Component {
             .then((response) => response.json())
             .then((res) => {
 
-
                 if (res.status === 1) {
                     this.setState({ login: true });
+
                 } else if (res.status === 0) {
                     this.setState({ login: false });
 
