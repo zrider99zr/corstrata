71fd983bbe5af4ebc1ad1010525bed3b7579bb4e
MarleyKreps <33332461+MarleyKreps@users.noreply.github.com>
2017-11-29 23:07:39 -0500

    11/29/17
    binding this.validate

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,7 +12,7 @@ class Home extends Component {
 
     componentDidMount() {
         console.log(sessionStorage.getItem("token"));
-        this.validateUser();
+        this.validateUser().bind(this);
     }
 
     validateUser() {
