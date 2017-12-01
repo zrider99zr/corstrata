20b196a5b5796eecefcd9c6004a43b50bf743f86
Zach Rider <zrider99zr@gmail.com>
2017-12-01 01:07:19 -0500

    Try that

--- src/pages/home.js
+++ src/pages/home.js
@@ -13,7 +13,7 @@ class Home extends Component {
             accountName: "",
             showTable: false,
         };
-        getAccountName();
+        this.getAccountName()
     }
 
     //========================================================================================================
